f7795980b6414418811ee3b7bace69cebb2a479e
Jeyhun Abilov <abilovv599@gmail.com>
2023-08-23 15:39:01 +0400

    done!

--- src/pages/Cart/Cart.jsx
+++ src/pages/Cart/Cart.jsx
@@ -1,7 +1,37 @@
+import { useOutletContext } from "react-router-dom";
+import { Card } from "../../components";
 import styles from "./Cart.module.scss";
 
 const Cart = () => {
-  return <div>Cart</div>;
+  const [favorites, cart, setToLocalStorage] = useOutletContext();
+  return (
+    <main className={styles.cart}>
+      <section>
+        <div className="container">
+          <div className={styles.row}>
+            {!cart.length ? (
+              <p className={styles.text}>No items in cart</p>
+            ) : (
+              cart.map((item) => (
+                <Card
+                  key={item.id}
+                  item={item}
+                  name={item.name}
+                  price={item.price}
+                  imageURL={item.imageURL}
+                  sku={item.sku}
+                  backgroundColor={item.backgroundColor}
+                  favorites={favorites}
+                  cart={cart}
+                  setToLocalStorage={setToLocalStorage}
+                />
+              ))
+            )}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export { Cart };
--- src/pages/Favorites/Favorites.jsx
+++ src/pages/Favorites/Favorites.jsx
@@ -1,7 +1,37 @@
-import styles from './Favorites.module.scss';
+import { useOutletContext } from "react-router-dom";
+import { Card } from "../../components";
+import styles from "./Favorites.module.scss";
 
 const Favorites = () => {
-  return <div>Favorites</div>;
+  const [favorites, cart, setToLocalStorage] = useOutletContext();
+  return (
+    <main className={styles.favorites}>
+      <section>
+        <div className="container">
+          <div className={styles.row}>
+            {!cart.length ? (
+              <p className={styles.text}>No items in favorites</p>
+            ) : (
+              favorites.map((item) => (
+                <Card
+                  key={item.id}
+                  item={item}
+                  name={item.name}
+                  price={item.price}
+                  imageURL={item.imageURL}
+                  sku={item.sku}
+                  backgroundColor={item.backgroundColor}
+                  favorites={favorites}
+                  cart={cart}
+                  setToLocalStorage={setToLocalStorage}
+                />
+              ))
+            )}
+          </div>
+        </div>
+      </section>
+    </main>
+  );
 };
 
 export { Favorites };
